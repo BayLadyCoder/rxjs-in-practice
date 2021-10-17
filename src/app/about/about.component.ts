2ef2ae2380fa56cb0cad32b2f472b6bb6ebd506a
BayLadyCoder <rachada_32@hotmail.com>
2021-10-17 00:24:34 -0400

    subscribe() method arguments, next(value), error(err), complete()

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -76,19 +76,31 @@ export class AboutComponent implements OnInit {
     // * and emits more values on every n milliseconds from the second argument
     // first argument is the wait time before it starts the first emission
     // second argument is the time interval to emit values after the first emission
-    const timer$ = timer(5000, 1000);
-    timer$.subscribe((value) => console.log("stream timer 1 " + value));
-    timer$.subscribe((value) => console.log("stream timer 2 " + value));
+    // const timer$ = timer(5000, 1000);
+    // timer$.subscribe((value) => console.log("stream timer 1 " + value));
+    // timer$.subscribe((value) => console.log("stream timer 2 " + value));
 
     // * Click Stream
     // * fromEvent() returns an observable which is the definition of a stream, it's a blueprint of a stream
     // first argument, source document or a specific button in the document is
     // second argument, the event name that we want to subscribe
-    const click$ = fromEvent(document, "click");
-    click$.subscribe((event) => console.log(event));
+    // const click$ = fromEvent(document, "click");
+    // click$.subscribe((event) => console.log(event));
 
     // ! These (interval$, timer$ and click$) are jus definitions of the streams
     // ! They are not the actual streams until it starts subscribe
     // ! After we subscribe to it, then we start to get the values emitted by these observables
+
+    // ? What other things we can so on the subscribe method ?
+    // subscribe(), returns a subscription that contains unsubscribe() method that we can use when we want to unsubscribe to an observable
+    // 1st argument, next(value), is called when we receive a value from the stream
+    // 2nd argument, error(err), is called when an error occurs, when this is called, the stream is closed
+    // 3rd argument, complete(), is called when the stream finishes emitting values, when this is called, the stream is closed
+    const click2$ = fromEvent(document, "click");
+    click2$.subscribe(
+      (event) => console.log("click2$ value", event),
+      (err) => console.log("click2$ You got an error ", err),
+      () => console.log("click2$ completed")
+    );
   }
 }
