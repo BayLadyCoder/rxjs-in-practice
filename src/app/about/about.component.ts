1805f9ccabc364a50dd8764c5d125e2790072943
BayLadyCoder <rachada_32@hotmail.com>
2021-10-17 00:10:55 -0400

    Different types of Streams of data/value(s)

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,40 +1,47 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import {
-    concat,
-    fromEvent,
-    interval,
-    noop,
-    observable,
-    Observable,
-    of,
-    timer,
-    merge,
-    Subject,
-    BehaviorSubject,
-    AsyncSubject,
-    ReplaySubject
-} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  observable,
+  Observable,
+  of,
+  timer,
+  merge,
+  Subject,
+  BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject,
+} from "rxjs";
+import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  ngOnInit() {
+    // * Different types of Streams of data/value(s
 
-    ngOnInit() {
+    // This one can emits multiple values and never complete
+    document.addEventListener("click", (event) => {
+      console.log(event);
+    });
 
+    let counter = 0;
 
-    }
+    // This one emits multiple values and never complete
+    setInterval(() => {
+      console.log(counter);
+      counter++;
+    }, 1000);
 
-
+    // This one emits value once and completes
+    setTimeout(() => {
+      console.log("finished...");
+    }, 3000);
+  }
 }
-
-
-
-
-
-
